1575e62c2366394f9d8219f16262dd3c9fe54ee6
AMIR <135604409+HESL0@users.noreply.github.com>
2025-01-22 14:46:19 +0330

    feat: add herobanner with ts dots and pointer (3.5hr)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import NavBar from "./components/NavBar";
+import HeroBanner from "./components/HeroBanner"
 
 function App() {
   return (
     <div>
       <NavBar />
+      <HeroBanner />
     </div>
   );
 }
